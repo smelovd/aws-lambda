c70a692520249141e3108aa10740ad9439122eb0
Denys Smielov <smelov.denis.2006@gmail.com>
2024-11-22 00:24:13 +0200

    fix

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "hello_world",
     roleName = "hello_world-role",
-    isPublishVersion = false,
+    isPublishVersion = true,
     aliasName = "${lambdas_alias_name}",
     logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
