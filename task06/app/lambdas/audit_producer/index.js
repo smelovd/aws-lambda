7ee8b7da448b13567f4c5b6d911d8bca9b614f3b
Denys Smielov <smelov.denis.2006@gmail.com>
2024-12-06 00:46:57 +0200

    task06

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -4,20 +4,36 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     console.log(JSON.stringify(event, null, 2))
-    item = {
-        id: uuidv4(),
-        itemKey: event.Records[0].dynamodb.Keys.key.S,
-        modificationTime: new Date().toISOString(),
-        newValue: {
-            key: event.Records[0].dynamodb.NewImage.key.S,
-            value: Number(event.Records[0].dynamodb.NewImage.value.N)
+    if (event.Records[0].eventName == "INSERT") {
+        item = {
+            id: uuidv4(),
+            itemKey: event.Records[0].dynamodb.Keys.key.S,
+            modificationTime: new Date().toISOString(),
+            newValue: {
+                key: event.Records[0].dynamodb.NewImage.key.S,
+                value: Number(event.Records[0].dynamodb.NewImage.value.N)
+            }
         }
+        console.log(JSON.stringify(item, null, 2))
+
+        await dynamodb.put({
+            TableName: "cmtr-e4ed9c72-Audit-test",
+            Item: item
+        }).promise();
+    } else if (event.Records[0].eventName == "MODIFY") {
+        item = {
+            id: uuidv4(),
+            itemKey: event.Records[0].dynamodb.Keys.key.S,
+            modificationTime: new Date().toISOString(),
+            updatedAttribute: event.Records[0].dynamodb.Keys.key.S,
+            oldValue: Number(event.Records[0].dynamodb.OldImage.value.N),
+            newValue: Number(event.Records[0].dynamodb.NewImage.value.N)
+        }
+        console.log(JSON.stringify(item, null, 2))
+
+        await dynamodb.put({
+            TableName: "cmtr-e4ed9c72-Audit-test",
+            Item: item
+        }).promise();
     }
-    console.log(JSON.stringify(item, null, 2))
-
-    await dynamodb.put({
-        TableName: "cmtr-e4ed9c72-Audit-test",
-        Item: item
-    }).promise();
-
 };
