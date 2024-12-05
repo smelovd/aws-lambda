f003ede297b055f06be425556daea13df264ed38
Denys Smielov <smelov.denis.2006@gmail.com>
2024-12-06 01:27:10 +0200

    task07

--- task07/app/lambdas/uuid_generator/index.js
+++ task07/app/lambdas/uuid_generator/index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
     // TODO implement
+    console.log(JSON.stringify(event, null, 2));
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
