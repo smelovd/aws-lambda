031f8d9e1c56a7fe724f2e1cf8254c224ddbf31b
Denys Smielov <smelov.denis.2006@gmail.com>
2024-12-05 23:54:40 +0200

    task06

---
+++ task06/app/lambdas/audit_producer/index.js
@@ -0,0 +1,7 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    console.log(JSON.stringify(event, null, 2))
+};
