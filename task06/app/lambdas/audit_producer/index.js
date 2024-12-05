18142c2b134576f03f6febfb3114a07c8b46383a
Denys Smielov <smelov.denis.2006@gmail.com>
2024-12-06 00:29:03 +0200

    task06

--- task06/app/lambdas/audit_producer/index.js
+++ task06/app/lambdas/audit_producer/index.js
@@ -4,4 +4,20 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     console.log(JSON.stringify(event, null, 2))
+    item = {
+        id: uuidv4(),
+        itemKey: event.Records[0].dynamodb.Keys.key.S,
+        modificationTime: new Date().toISOString(),
+        newValue: {
+            key: event.Records[0].dynamodb.NewImage.key.S,
+            value: event.Records[0].dynamodb.NewImage.value.N
+        }
+    }
+    console.log(JSON.stringify(item, null, 2))
+
+    await dynamodb.put({
+        TableName: "cmtr-e4ed9c72-Audit-test",
+        Item: item
+    }).promise();
+
 };
