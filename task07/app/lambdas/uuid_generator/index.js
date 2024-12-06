6fe2ce282f917e2e0396cf169195353d93f29079
Denys Smielov <smelov.denis.2006@gmail.com>
2024-12-06 02:16:40 +0200

    task07

--- task07/app/lambdas/uuid_generator/index.js
+++ task07/app/lambdas/uuid_generator/index.js
@@ -3,28 +3,23 @@ const { v4: uuidv4 } = require('uuid');
 const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
-    // TODO implement
-    console.log(JSON.stringify(event, null, 2));
-    const body = {
-        "ids": [
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4(),
-            uuidv4()
-        ]
+    try {
+        console.log(JSON.stringify(event, null, 2));
+
+        const body = {
+            "ids": Array.from({ length: 10 }, () => uuidv4())
+        };
+
+        const params = {
+            Bucket: "cmtr-e4ed9c72-uuid-storage-test",
+            Key: new Date().toISOString() + '.json', 
+            Body: JSON.stringify(body), 
+            ContentType: "application/json"
+        };
+
+        await s3.putObject(params).promise();
+
+    } catch (error) {
+        console.error(error);
     }
-
-    const params = {
-        Bucket: "cmtr-e4ed9c72-uuid-storage-test",
-        Key: new Date().toISOString() + '.json',
-        Body: Json.stringify(body),
-    };
-
-    await s3.putObject(params).promise();
 };
