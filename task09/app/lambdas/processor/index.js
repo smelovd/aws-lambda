906abb254a6878a24b963ad5343ad48cfaf07485
Denys Smielov <smelov.denis.2006@gmail.com>
2024-12-06 13:47:54 +0200

    task09

--- task09/app/lambdas/processor/index.js
+++ task09/app/lambdas/processor/index.js
@@ -8,40 +8,46 @@ const WEATHER_TABLE = "Weather";
 
 exports.handler = async (event) => {
     try {
-      const { latitude, longitude } = event.queryStringParameters || {};
-  
-      if (!latitude || !longitude) {
-        return {
-          statusCode: 400,
-          body: JSON.stringify({ error: "Missing 'latitude' or 'longitude' parameter" }),
-        };
-      }
-  
-      AWSXRay.captureAsyncFunc('fetchWeatherData', async (subsegment) => {
-        const response = await axios.get(`https://api.open-meteo.com/v1/forecast`, {
-          params: { latitude, longitude, hourly: "temperature_2m,time" },
+        console.log("Event:", event);
+        const { latitude, longitude } = event.queryStringParameters || {};
+        console.log(latitude, longitude);
+
+        if (!latitude || !longitude) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Missing 'latitude' or 'longitude' parameter" }),
+            };
+        }
+
+        AWSXRay.captureAsyncFunc('fetchWeatherData', async (subsegment) => {
+            const response = await axios.get(`https://api.open-meteo.com/v1/forecast`, {
+                params: { latitude, longitude, hourly: "temperature_2m,time" },
+            });
+            subsegment.close();
+            return response;
         });
-        subsegment.close();
-        return response;
-      });
-  
-      const weatherData = response.data;
-  
-      const item = { id: uuidv4(), forecast: weatherData };
-  
-      await dynamodb
-        .put({ TableName: WEATHER_TABLE, Item: item })
-        .promise();
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Weather data saved successfully", item }),
-      };
+
+        const weatherData = response.data;
+        console.log("Weather Data:", weatherData);
+
+        const item = { id: uuidv4(), forecast: weatherData };
+
+        console.log("Item:", item);
+        await dynamodb
+            .put({ TableName: WEATHER_TABLE, Item: item })
+            .promise();
+
+        console.log("Item saved:", item);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Weather data saved successfully", item }),
+        };
     } catch (error) {
-      console.error("Error:", error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: "Internal Server Error" }),
-      };
+        console.error("Error:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal Server Error" }),
+        };
     }
-  };
+};
